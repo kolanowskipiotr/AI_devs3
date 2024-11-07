994a8be9dd0a33e243d130ef36d2b7382e236f88
Piotr Kolanowski <piotr.kolanowski@invia.pl>
2024-11-07 11:17:56 +0100

    Repackage

--- src/main/scala/pl/pko/ai/devs3/ollama/model/OllamaResponse.scala
+++ src/main/scala/pl/pko/ai/devs3/ollama/model/OllamaResponse.scala
@@ -1,12 +1,12 @@
-package pl.pko.ai.devs3.s01.e01
+package pl.pko.ai.devs3.ollama.model
 
 case class OllamaResponse(
   response: String
 )
 
 object OllamaResponse {
-  import io.circe.{Decoder, Encoder}
   import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+  import io.circe.{Decoder, Encoder}
 
   implicit val encoder: Encoder[OllamaResponse] = deriveEncoder[OllamaResponse]
   implicit val decoder: Decoder[OllamaResponse] = deriveDecoder[OllamaResponse]
--- src/main/scala/pl/pko/ai/devs3/s01/e01/GoogleSerchResult.scala
+++ src/main/scala/pl/pko/ai/devs3/s01/e01/GoogleSerchResult.scala
@@ -1,5 +1,7 @@
 package pl.pko.ai.devs3.s01.e01
 
+import pl.pko.ai.devs3.ollama.model.OllamaResponse
+
 case class GoogleSerchResult(
   ollamaResponse: OllamaResponse,
   googleResponse: String
